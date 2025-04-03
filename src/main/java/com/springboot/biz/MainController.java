8bff01cea0e729c10750d877814f1db10ede7308
hama82zien <wngjsrb88@gmail.com>
2025-04-03 23:28:04 +0900

    액션 테스트

---
+++ src/main/java/com/springboot/biz/MainController.java
@@ -0,0 +1,13 @@
+package com.springboot.biz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String root() {
+		return "index";
+	}
+}
