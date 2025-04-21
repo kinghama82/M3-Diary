bcb047887657f3d08f7dde86952e20b295bb1368
sungyoung <jeung2741@gmail.com>
2025-04-21 15:54:43 +0900

    미완성 중간 저장

--- src/main/java/com/springboot/biz/m3user/M3Controller.java
+++ src/main/java/com/springboot/biz/m3user/M3Controller.java
@@ -18,37 +18,41 @@ public class M3Controller {
 
     private final M3Service m3Service;
 
-    @GetMapping("/loging")
+    @GetMapping("/signup")
     public String signup(M3userForm m3userForm) {
-        return "loging";
+        return "signup";
     }
 
-    @PostMapping("/loging")
-    public String loging(@Valid M3userForm m3userForm, BindingResult bindingResult) {
+    @PostMapping("/signup")
+    public String signupSubmit(@Valid M3userForm m3userForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "loging";
+            return "signup";
         }
         if (!m3userForm.getPassword().equals(m3userForm.getPassword2())) {
             bindingResult.rejectValue("password2", "password오류",
                     "비밀번호가 틀립니다.");
-            return "loging";
+            return "signup";
         }
         try {
             m3Service.create(m3userForm.getUsername(),m3userForm.getPassword(),m3userForm.getNickname(),
                     m3userForm.getEmail(),m3userForm.getBirthday(),
-                    m3userForm.getAddress(),m3userForm.getPhone(),m3userForm.getAddressDetail());
+                    m3userForm.getAddress(),m3userForm.getPhone(),m3userForm.getAddressDetail(), m3userForm.getPassword2());
         }catch (DataIntegrityViolationException e) {
             e.printStackTrace();
 
         bindingResult.reject("logingFailed", "중복된 사용자 입니다.");
-        return "loging";
+        return "signup";
     }catch (Exception e) {
             e.printStackTrace();
             bindingResult.reject("logingFailed", e.getMessage());
-            return "loging";
+            return "signup";
         }
         return "redirect:/user/login";
 
+    }
+    @GetMapping("/login")
+    public String loginpage() {
+        return "login";
     }
 
 
--- src/main/java/com/springboot/biz/m3user/M3Service.java
+++ src/main/java/com/springboot/biz/m3user/M3Service.java
@@ -2,6 +2,7 @@ package com.springboot.biz.m3user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -9,12 +10,14 @@ public class M3Service {
 
     private final M3Repository m3Repository;
 
+@Transactional
     public M3User create(String email, String password, String nickname, String phone, String birthday
-    , String address, String addressDetail, String username) {
+    , String address, String addressDetail, String username, String password2) {
         M3User m3User = new M3User();
         m3User.setEmail(email);
         m3User.setUsername(username);
         m3User.setPassword(password);
+       /* m3User.setPassword2(password2);*/
         m3User.setNickname(nickname);
         m3User.setPhone(phone);
         m3User.setBirthday(birthday);
