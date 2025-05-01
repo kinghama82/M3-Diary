82048139c1d95fb92358f01961cdb9955b64ef74
hama82zien <wngjsrb88@gmail.com>
2025-05-02 02:14:53 +0900

    정부api

--- src/main/java/com/springboot/biz/government/GovernmentController.java
+++ src/main/java/com/springboot/biz/government/GovernmentController.java
@@ -1,9 +1,11 @@
 package com.springboot.biz.government;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,10 +19,16 @@ public class GovernmentController {
 	@Value("${gov.api-key}")
 	private String apikey;
 	
+	private final RestTemplate restTemplate = new RestTemplate();
+	
+	private String apiBaseUrl = "https://www.youthcenter.go.kr/go/ythip/getPlcy";
 	
 	@GetMapping("/list")
-	public void list() {
+	public ResponseEntity<?> list() {
+		String apiUrl = apiBaseUrl + "?apiKeyNm=" + apikey + "&rtnType=json";
 		
+		Object res = restTemplate.getForObject(apiUrl, Object.class);
+		return ResponseEntity.ok(res);
 	}
 	
 	
