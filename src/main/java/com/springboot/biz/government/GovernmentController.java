1925352b82f8f2da3b0e01afa979b079ef4cd0e2
hama82zien <wngjsrb88@gmail.com>
2025-05-02 01:32:44 +0900

    ci/cd 제발좀

--- src/main/java/com/springboot/biz/government/GovernmentController.java
+++ src/main/java/com/springboot/biz/government/GovernmentController.java
@@ -2,11 +2,13 @@ package com.springboot.biz.government;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/gov")
+@RequestMapping("/api/gov")
 public class GovernmentController {
 
 	private final GovernmentService governmentService;
